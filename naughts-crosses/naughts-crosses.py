dc8878803bb8cc891896fb8026541df767b11166
Carlie Thomas <carlierobinson88@yahoo.com>
2019-06-24 16:49:15 +0100

    Adjust code to fit pep8 guidelines

--- naughts-crosses/naughts-crosses.py
+++ naughts-crosses/naughts-crosses.py
@@ -1,6 +1,7 @@
 import random
 # create the board
 
+
 def display_board(board):
     print('\n'*50)
     print('   |   |')
@@ -17,10 +18,12 @@ def display_board(board):
 
 # Define game players names
 
-player1_name = input('What is your name player 1? ')
+player1_name = input('\nWhat is your name player 1? ')
 player2_name = input('\nWhat is your name player 2? ')
 
+
 # Players choose their playing piece
+
 
 def player_input():
     marker = ''
@@ -36,6 +39,7 @@ def player_input():
     return (player1, player2)
 
 # First or second
+
 
 def first_or_second():
 
@@ -55,40 +59,43 @@ def first_or_second():
 
 # Choose where to place your first move
 
+
 def place_marker(board, marker, position):
 
     board[position] = marker
 
 # Check whether anyone has won the game
 
+
 def check_winner(board, mark):
     return ((board[1] == board[2] == board[3] == mark) or
-    (board[4] == board[5] == board[6] == mark) or
-    (board[7] == board[8] == board[9] == mark) or
-    (board[7] == board[4] == board[1] == mark) or
-    (board[8] == board[5] == board[2] == mark) or
-    (board[9] == board[6] == board[3] == mark) or
-    (board[9] == board[5] == board[1] == mark) or
-    (board[7] == board[5] == board[3] == mark))
+            (board[4] == board[5] == board[6] == mark) or
+            (board[7] == board[8] == board[9] == mark) or
+            (board[7] == board[4] == board[1] == mark) or
+            (board[8] == board[5] == board[2] == mark) or
+            (board[9] == board[6] == board[3] == mark) or
+            (board[9] == board[5] == board[1] == mark) or
+            (board[7] == board[5] == board[3] == mark))
 
 # Check whether a space is free
 
-def space_check (board, position):
+
+def space_check(board, position):
 
     return ((board[position] == '1') or
-    (board[position] == '2') or
-    (board[position] == '3') or
-    (board[position] == '4') or
-    (board[position] == '5') or
-    (board[position] == '6') or
-    (board[position] == '7') or
-    (board[position] == '8') or
-    (board[position] == '9'))
+            (board[position] == '2') or
+            (board[position] == '3') or
+            (board[position] == '4') or
+            (board[position] == '5') or
+            (board[position] == '6') or
+            (board[position] == '7') or
+            (board[position] == '8') or
+            (board[position] == '9'))
 
     return True
-    
 
 # Check if board is full
+
 
 def full_board_check(board):
     for i in range(1, 10):
@@ -99,14 +106,16 @@ def full_board_check(board):
 
 # Player chooses a position
 
+
 def player_choice(board):
 
     position = 0
 
     while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('\nPlease enter a position to place your piece (1-9): '))
-    
+
     return position
+
 
 def replay():
 
@@ -115,24 +124,22 @@ def replay():
     if yes_or_no[0] == 'y':
         return True
 
-    
-
 # While loop that keeps the game running
 
-print ('\nWELCOME TO NAUGHTS AND CROSSES!!')
+print('\nWELCOME TO NAUGHTS AND CROSSES!!')
 
 while True:
     # Play the game
 
-    ## Set everything up (board, name, who's first, marker choice)
+    # Set everything up (board, name, who's first, marker choice)
 
     the_board = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     player1, player2 = player_input()
 
     turn = first_or_second()
-    print (turn)
+    print(turn)
 
-    play_game = input('\nAre you ready to start the game? ')
+    play_game = input(f'\n{player1_name} and {player2_name} are you ready to start the game? ')
 
     if play_game.lower() == 'yes':
         game_on = True
@@ -152,21 +159,19 @@ while True:
             # Check if they won
             if check_winner(the_board, player1):
                 display_board(the_board)
-                print (f'\n{player1_name} congratulations, you have won!!')
+                print(f'\n{player1_name} congratulations, you have won!!')
                 game_on = False
             else:
                 if full_board_check(the_board):
                     display_board(the_board)
-                    print ('\nThis game is a tie!')
+                    print('\nThis game is a tie!')
                     game_on = False
                 else:
                     turn = player2_name
-            
 
             # If no tie and no win next player's turn
 
-
-        ### First player turn
+        # First player turn
 
         else:
 
@@ -179,12 +184,12 @@ while True:
             # Check if they won
             if check_winner(the_board, player2):
                 display_board(the_board)
-                print (f'\n{player2_name} congratulations, you have won!!')
+                print(f'\n{player2_name} congratulations, you have won!!')
                 game_on = False
             else:
                 if full_board_check(the_board):
                     display_board(the_board)
-                    print ('\nThis game is a tie!')
+                    print('\nThis game is a tie!')
                     game_on = False
                 else:
                     turn = player1_name
@@ -192,4 +197,3 @@ while True:
     if not replay():
         break
 # Break out of the while loop while on replay()
-
