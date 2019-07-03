0a569ea3dc6a574ea3bfef6b56b21a02c6616b7e
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-03 18:08:28 +0100

    Add class that returns suits and ranks of a card

--- black-jack/black-jack.py
+++ black-jack/black-jack.py
@@ -10,3 +10,13 @@ values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5,
           'Jack': 10, 'Queen': 10, 'King': 10}
 
 playing = True
+
+class Card:
+
+    def __init__(self, suit, rank)
+
+         self.suit = suit
+         self.rank = rank
+
+    def __str__ (self):
+        return self.rank + 'of' + self.suit
