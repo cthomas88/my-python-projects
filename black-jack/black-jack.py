b7eee9500db7eeded2a5fdb5425d3f815c17fe68
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-03 18:18:03 +0100

    Add Deck class
    Creates a random list of suits and ranks then produces one
    random card. Also prints remainder of deck

--- black-jack/black-jack.py
+++ black-jack/black-jack.py
@@ -20,3 +20,25 @@ class Card:
 
     def __str__ (self):
         return self.rank + 'of' + self.suit
+
+class Deck:
+
+    def __init__ (self):
+        self.deck = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append (Card(suit, rank)) # Builds card objects and adds them to a list
+    
+    def __str__ (self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n ' +card.__str__()
+        return "The deck has: " + deck_comp
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+    
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
