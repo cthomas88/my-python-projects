b7e783070363cdef7e4dc97cbc0946499177fcbe
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-03 18:00:04 +0100

    Add playing variable set to True

--- black-jack/black-jack.py
+++ black-jack/black-jack.py
@@ -2,7 +2,11 @@ import random
 
 suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
 
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Ace', 'Jack', 'Queen', 'King')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
+         'Ten', 'Ace', 'Jack', 'Queen', 'King')
 
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 
-'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Ace': 11, 'Jack': 10, 'Queen': 10, 'King': 10}
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5,
+          'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Ace': 11,
+          'Jack': 10, 'Queen': 10, 'King': 10}
+
+playing = True
