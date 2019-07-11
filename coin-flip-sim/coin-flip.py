48737da6da9548704e382e14f3db7b52e7444cc0
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-11 12:00:07 +0100

    Edit coin todd func

--- coin-flip-sim/coin-flip.py
+++ coin-flip-sim/coin-flip.py
@@ -1,7 +1,7 @@
 import random
 
 # Global variables
-user_number = input('Please enter the number of times you would like the computer to flip your coin: ')
+user_number = int(input('Please enter the number of times you would like the computer to flip your coin: '))
 
 
 # Function that produces a head or tails
@@ -11,11 +11,13 @@ def coin_toss():
 
     heads_or_tails = random.randint(0, 51)
 
-    if heads_or_tails % 2 == 0:
+    if heads_or_tails %2 == 0:
         heads_or_tails = 'heads'
     else:
         heads_or_tails = 'tails'
+    
     return heads_or_tails
 
+def number_of_throws():
 
-
+    
