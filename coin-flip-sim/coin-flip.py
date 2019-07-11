0d82a2bc3713d5ddd15d8967858b6b51edb21e4a
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-11 13:29:15 +0100

    Tidy up code as per pep8

--- coin-flip-sim/coin-flip.py
+++ coin-flip-sim/coin-flip.py
@@ -1,14 +1,16 @@
 import random
 
-def coin_flip():
 
-    flip = random.randint(0, 51)
+def flip():
 
-    if flip % 2 == 0:
+    flips = random.randint(0, 51)
+
+    if flips % 2 == 0:
         return True
     else:
         return False
-    
+
+
 def game(num):
 
     heads = 0
@@ -16,16 +18,16 @@ def game(num):
     list_results = []
 
     for i in range(num):
-        if coin_flip():
+        if flip():
             heads += 1
             list_results.append('Heads')
         else:
             tails += 1
             list_results.append('Tails')
-    
-    print(f'The number of times the coin landed heads was {heads}')
-    print(f'The number of times the coin landed tails was {tails}')
+
+    print(f"The number of times the coin landed on heads was {heads}")
+    print(f"The number of times the coin landed on tails was {tails}")
     print(list_results)
 
-number_of_throws = int(input('How many times would you like to toss the coin? '))
-game(number_of_throws)
+user_input = int(input('Please select the number of times you would like to throw the coin: '))
+game(user_input)
