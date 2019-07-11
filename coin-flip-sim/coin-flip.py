aaa2a0cf0a96e5983f7900869e9eb3bc31da4dd1
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-11 11:45:56 +0100

    Add heads or tails function

--- coin-flip-sim/coin-flip.py
+++ coin-flip-sim/coin-flip.py
@@ -1,8 +1,21 @@
 import random
 
 # Global variables
+user_number = input('Please enter the number of times you would like the computer to flip your coin: ')
 
-# Function that allows user input for number of flips
 
-# Coin flip function 
+# Function that produces a head or tails
+
+
+def coin_toss():
+
+    heads_or_tails = random.randint(0, 51)
+
+    if heads_or_tails % 2 == 0:
+        heads_or_tails = 'heads'
+    else:
+        heads_or_tails = 'tails'
+    return heads_or_tails
+
+
 
