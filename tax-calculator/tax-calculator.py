028e769361c06a6fb613da1282f152815e06f8a6
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-12 09:43:23 +0100

    Clean up code

--- tax-calculator/tax-calculator.py
+++ tax-calculator/tax-calculator.py
@@ -4,7 +4,7 @@ def tax_calc(cost, tax):
 
     print(f'\nThe total amount to pay is ${total:.2f}')
 
-users_item = float(input('How much does your item cost? '))
+users_item = float(input('How much does your item cost? $'))
 
 users_tax = float(input('\nHow much is the tax in your area? '))
 
