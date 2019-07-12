fec6ad504570e6d200dd0c1fea5751f4dd2f93a3
Carlie Thomas <carlierobinson88@yahoo.com>
2019-07-12 09:34:02 +0100

    Create tax calc func and call function

---
+++ tax-calculator/tax-calculator.py
@@ -0,0 +1,11 @@
+def tax_calc(cost, tax):
+
+    total = cost * tax
+
+    print(f'\nThe total amount to pay is ${total:.2f}')
+
+users_item = float(input('How much does your item cost? '))
+
+users_tax = float(input('\nHow much is the tax in your area? '))
+
+tax_calc(users_item, users_tax)
